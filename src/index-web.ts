0c429d7900257c3d869e49dabac740244b64074c
Zachary Belford <belfordz66@gmail.com>
2020-09-08 14:37:18 -0700

    fix: browser support issues resolved
    fixes #60

--- src/index-web.ts
+++ src/index-web.ts
@@ -4,4 +4,5 @@ import fetch from "isomorphic-fetch";
 export default buildDereferencer(fetch, {
   access: () => Promise.resolve(false),
   readFile: () => Promise.resolve(""),
+  constants: { F_OK: 0, R_OK: 0 }, //tslint:disable-line
 });
